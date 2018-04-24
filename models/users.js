1b67513c7e27f7e60181f1004db26d58a4b58547
Edgar Contreras <ec542498@gmail.com>
2018-04-23 21:45:49 -0430

    Agregando Sequelize

---
+++ models/users.js
@@ -0,0 +1,47 @@
+module.exports = (sequelize, DataType)=>{
+
+    const Users = sequelize.define('Users', {
+  
+      id: {
+        type: DataType.INTEGER,
+        primaryKey: true,
+        autoIncrement: true
+      },
+  
+      name: {
+        type: DataType.STRING,
+        allowNull: false,
+        validate: {
+          notEmpty: false
+        }
+      },
+
+      password: {
+        type: DataType.STRING,
+        allowNull: false,
+        validate: {
+          notEmpty: false
+        }
+      },
+
+      email: {
+        type: DataType.STRING,
+        unique: true,
+        allowNull: false,
+        validate: {
+            notEmpty: true
+        }
+      }
+  
+  
+    }, {
+      
+      classMethods: {
+        associate: (models) => {
+          Users.hasMany(models.Tasks)
+        }
+      }
+    })
+  
+    return Users
+  }
